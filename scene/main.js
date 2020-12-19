1c2fc1efcf0bf151b309e8f281da0fd64216a504
Mike McDonald <whoiswizard@icloud.com>
2020-12-19 09:49:00 -0600

    added scene-it movie search project beginnings

---
+++ scene/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    console.log('Ready!');
+
+const renderMovies = movieArray => {
+    console.log('rendering');
+  let movieHTML = movieArray.map(function(currentMovie){
+
+    });
+    return finalHTML.join('');
+}
+
+renderMovies(movieData);
+
+});
