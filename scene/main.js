30282fe489ad209d6f802ab96f6b7ad23bd9b3d6
Mike McDonald <whoiswizard@icloud.com>
2020-12-21 22:56:19 -0600

    stubbed out beginnings of watchlist functionality

--- scene/main.js
+++ scene/main.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
     console.log('Ready!');
-
+    
+  
+    // const saveToWatchlist = (imdbID) => {
+    //     console.log('save to watchlist');
+    //     console.log(imdbID);
+    // };
+    $('#movie-results').on('click', '.addBtn', function(){
+        console.log('add to watchlist');
+    });
     
 $('#searchButton').click(function (event) {
   event.preventDefault();
@@ -12,17 +20,14 @@ $('#searchButton').click(function (event) {
   const $jumboTron = $('.jumbotron');
 
   $.get(`https://www.omdbapi.com/?apikey=9c815c55&s=${$searchString}`).done(function(response){
-  console.log(response)  ;
-  // response.Search.forEach(function(movie){
-  //     // console.log(movie);
-  //     // console.log(movie.Title);      
-  //   });
-  renderMovies(response.Search);
+    console.log(response);
+    renderMovies(response.Search);
   });
 
   const renderMovies = (movieArray) => {
-   $jumboTron.css("display","none")
+    $jumboTron.css("display","none")
     $movieResults.html('');
+   
     movieArray.map((movie) => {
       $movieResults.append(
       `<div class="col-md-4">
@@ -37,7 +42,7 @@ $('#searchButton').click(function (event) {
               <div class="d-flex justify-content-between align-items-center">
                   <div class="btn-group">
                       <a role="button" class="btn btn-sm btn-outline-secondary" href="https://www.imdb.com/title/${movie.imdbID}" target="_blank">View</a>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Add</button>
+                      <button  type="button" class="btn btn-sm btn-outline-secondary addBtn">Add</button>
                   </div>
                   
               </div>
